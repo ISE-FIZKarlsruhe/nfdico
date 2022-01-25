8b7f313819aa921bda80d85305e669ca10d219ac
Etienne Posthumus <eposthumus@gmail.com>
2022-01-25 18:37:33 +0100

    Alternate backends amd remove YASGUI

--- www/shmarql.py
+++ www/shmarql.py
@@ -15,6 +15,11 @@ PREFIX = {
     "http://www.w3.org/2002/07/owl#": "owl:",
 }
 
+triplestore_map = {
+    "fuseki": "/fuseki/sparql",
+    "blazegraph": "/bigdata/namespace/kb/sparql",
+}
+
 
 def find_attr_parents(element, attr):
     val = element.getAttribute(attr)
@@ -72,15 +77,33 @@ def snippet(param, elem):
     return d.render()
 
 
-def display(results):
+def display_query(results):
+    thead_tr = Tr()
+    for headvar in results["head"]["vars"]:
+        thead_tr.C.append(Td(headvar))
+    tbody = Tbody()
+    for row in results["results"]["bindings"]:
+        arow = Tr()
+        for k, v in dict(row).items():
+            arow.C.append(Td(v["value"]))
+        tbody.C.append(arow)
+    t = Table(
+        Thead(thead_tr),
+        tbody,
+        _class="table table-striped",
+    )
+    document.getElementById("query_results").innerHTML = t.render()
+
+
+def display_explore(results):
     if "results" in results and "bindings" in results["results"]:
         results = results["results"]["bindings"]
     t = Table(
         Thead(
             Tr(
-                Td(Span(document.curr_s, _class="btn btn-sm", uri="?s", wanted="S")),
-                Td(Span(document.curr_p, _class="btn btn-sm", uri="?p", wanted="P")),
-                Td(Span(document.curr_o, _class="btn btn-sm", uri="?o", wanted="O")),
+                Th(Span(document.curr_s, _class="btn btn-sm", uri="?s", wanted="S")),
+                Th(Span(document.curr_p, _class="btn btn-sm", uri="?p", wanted="P")),
+                Th(Span(document.curr_o, _class="btn btn-sm", uri="?o", wanted="O")),
             )
         ),
         _class="table table-striped",
@@ -106,8 +129,11 @@ def display(results):
     results_element.innerHTML = buf
 
 
-async def sparql(endpoint, q):
+async def sparql(q):
     __pragma__("jsiter")
+    ts = get_endpoint()
+    endpoint = triplestore_map.get(ts, "/fuseki/sparql")
+
     res = await fetch(
         endpoint,
         {
@@ -119,6 +145,10 @@ async def sparql(endpoint, q):
             "body": "query=" + encodeURIComponent(q),
         },
     )
+    if res.status != 200:
+        txt = await res.text()
+        return {"error": txt}
+
     return await res.json()
     __pragma__("nojsiter")
 
@@ -128,9 +158,22 @@ def flash(msg):
     status_box.style.display = "block"
 
 
+def get_endpoint():
+    ts = "fuseki"
+    for elem in document.getElementsByName("triplestores"):
+        if elem.checked == True:
+            return elem.value
+
+
 async def change_endpoint():
-    ep = "https://nfdi.fiz-karlsruhe.de/nfdico/sparql"
-    results_element.innerHTML = '<div style="margin-top: 20px" class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></div>'
+    # ep = "https://nfdi.fiz-karlsruhe.de/nfdico/sparql"
+    ts = get_endpoint()
+
+    results_element.innerHTML = (
+        '<div style="margin-top: 20px" class="spinner-border" role="status">'
+        + ts
+        + '<span class="visually-hidden">Loading...</span></div>'
+    )
 
     q = "SELECT * WHERE { "
     if document.curr_s != "?s":
@@ -145,10 +188,10 @@ async def change_endpoint():
         q += "<" + document.curr_o + ">"
     else:
         q += document.curr_o
-    q += " } ORDER BY DESC(?s) LIMIT 1000"
+    q += " } ORDER BY ?s LIMIT 1000"
 
-    results = await sparql(ep, q)
-    display(results)
+    results = await sparql(q)
+    display_explore(results)
 
 
 async def results_clicker(event):
@@ -164,6 +207,14 @@ async def results_clicker(event):
     change_endpoint()
 
 
+async def execute_sparql_clicker(event):
+    results = await sparql(editor.getValue())
+    if "error" in results:
+        document.getElementById("query_results").innerHTML = results["error"]
+    else:
+        display_query(results)
+
+
 async def navtabs_clicker(event):
     event.preventDefault()
     document.getElementById("sprql").style.display = "none"
@@ -175,13 +226,28 @@ async def navtabs_clicker(event):
     if len(tabid) > 2:
         elemt = document.getElementById(tabid)
         elemt.style.display = "block"
+        if tabid == "shmrql":
+            change_endpoint()
+
+
+class E:
+    def preventDefault():
+        pass
 
 
 async def init():
+    execute_sparql = document.getElementById("execute_sparql")
+    execute_sparql.addEventListener("click", execute_sparql_clicker)
     navtabs = document.getElementById("navtabs")
     navtabs.addEventListener("click", navtabs_clicker)
     results_element.addEventListener("click", results_clicker)
-    change_endpoint()
+    if window.location.hash == "#query":
+        e = E()
+        e.target = document.getElementById("tabsprql")
+        navtabs_clicker(e)
+    else:
+        document.getElementById("sprql").style.display = "none"
+        change_endpoint()
 
 
 window.addEventListener("load", init)
