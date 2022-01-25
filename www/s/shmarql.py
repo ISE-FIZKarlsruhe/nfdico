618ecb6d3ebbb5f3ed08d1b2429c5395d8f59bcd
Etienne Posthumus <eposthumus@gmail.com>
2022-01-25 11:19:11 +0100

    Add owl: prefix

--- www/s/shmarql.py
+++ www/s/shmarql.py
@@ -12,6 +12,7 @@ PREFIX = {
     "https://www.nfdi4culture.de/resource/": "nfdires:",
     "http://www.w3.org/1999/02/22-rdf-syntax-ns#": "rdf:",
     "http://www.w3.org/2000/01/rdf-schema#": "rdfs:",
+    "http://www.w3.org/2002/07/owl#": "owl:",
 }
 
 
